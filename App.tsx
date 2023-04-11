01278a18a98db5533919d867fc04383194582980
balaev.yr <balaev.yr@alidi.ru>
2023-04-11 07:19:19 +0500

    #dc-14

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import AppNavigator from './source/navigation/rootNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'default'} />
