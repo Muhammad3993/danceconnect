322d51a1e1cd621ae1bd2afa9a06e663d054963b
Muhammad3993 <muhammadjon3993@gmail.com>
2024-07-20 03:35:39 +0500

    CHanged HomeScreen

--- src/screens/home_tabs/home/index.tsx
+++ src/screens/home_tabs/home/index.tsx
@@ -1,12 +1,12 @@
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import { theming } from 'common/constants/theming';
-import { images } from 'common/resources/images';
-import { LocationIcon } from 'components/icons/location';
 import { DCRoundIcon } from 'components/shared/round_icon';
 import { RightArrowIcon } from 'components/icons/rightArrow';
-import { SetCalendarIcon } from 'components/icons/setCalendar';
-import { HomeItem } from 'components/shared/homeItem';
+import { HomeItem } from 'components/shared/home_item';
+import { StartCommunity } from 'components/shared/start_community';
+import { CommunitiesIcon } from 'components/icons/communities';
+import { DCButton } from 'components/shared/button';
 
 export function HomeScreen() {
   return (
@@ -24,10 +24,32 @@ export function HomeScreen() {
         />
       </View>
 
-      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.homeBoxes}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           <HomeItem />
           <HomeItem />
       </ScrollView>
+
+      <StartCommunity />
+
+      <View style={styles.homeEvents}>
+        <Text style={styles.homeEventsTitle}>Your upcoming events</Text>
+        <View style={styles.homeEventsFree}>
+          <DCRoundIcon 
+            icon={<CommunitiesIcon active fill={theming.colors.white} />}
+            iconBoxStyle={{
+              width: 44,
+              height: 44,
+              backgroundColor: theming.colors.purple,
+            }}
+          />
+          <Text style={styles.homeEventsFreeTitle}>Join a community </Text>
+          <Text style={styles.homeEventsFreeSubtitle}>to see and attend upcoming events that you interested in</Text>
+          <View style={{marginTop: 15}}>
+            <DCButton children="Search for a community" variant='primary' containerStyle={{paddingHorizontal: theming.spacing.MD, paddingVertical: 13}}   />
+          </View>
+        </View>
+      </View>
+
     </ScrollView>
   );
 }
@@ -49,11 +71,39 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: theming.colors.textPrimary,
   },
-  homeBoxes: {
+  homeEvents: {
+    marginVertical: 30,
+  },
+  homeEventsTitle: {
+    color: theming.colors.textPrimary,
+    fontSize: 20,
+    fontWeight: "700",
+    fontFamily: theming.fonts.latoRegular,
+  },
+  homeEventsFree: {
+    width: "100%",
+    height: 236,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: theming.colors.gray250,
+    borderRadius: theming.spacing.SM,
     marginTop: 20,
   },
-
-
-
-
+  homeEventsFreeTitle: {
+    color: theming.colors.textPrimary,
+    fontSize: 18,
+    fontWeight: "700",
+    fontFamily: theming.fonts.latoRegular,
+    marginTop: 7,
+  },
+  homeEventsFreeSubtitle: {
+    width: "90%",
+    color: theming.colors.gray700,
+    fontWeight: "400",
+    fontSize: 14,
+    fontFamily: theming.fonts.latoRegular,
+    marginTop: 3,
+    textAlign: "center",
+  }
 });
