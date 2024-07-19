89794225cbd4edcd75c81db4dcd11e19c86f91f2
Muhammad3993 <muhammadjon3993@gmail.com>
2024-07-19 20:49:16 +0500

    HomeScreen Scroll Products

--- src/common/resources/images.ts
+++ src/common/resources/images.ts
@@ -5,4 +5,6 @@ export const images = {
   appleLogo: require('../../../assets/images/appleicon.png'),
   eventBg: require('../../../assets/images/eventbg.png'),
   eventAvatar: require('../../../assets/images/eventavatar.png'),
+  homeImg: require('../../../assets/images/rectangle.png'),
+  homeImg1: require('../../../assets/images/rectangle1.png'),
 };
