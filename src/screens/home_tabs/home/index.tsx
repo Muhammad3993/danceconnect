3f40760ba968c8166c82d56c0c94785372b132a7
JavoxirNazarov <mrzziskandarov96>
2024-07-22 15:26:07 +0500

    refactor home

--- src/screens/home_tabs/home/index.tsx
+++ src/screens/home_tabs/home/index.tsx
@@ -1,20 +1,97 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React, { useState } from 'react';
 import { theming } from 'common/constants/theming';
 import { DCRoundIcon } from 'components/shared/round_icon';
 import { RightArrowIcon } from 'components/icons/rightArrow';
 import { HomeItem } from 'components/shared/home_item';
 import { StartCommunity } from 'components/shared/start_community';
-import { HomeEvent } from 'components/home_event';
-import { useDCStore } from 'store';
+import { useTranslation } from 'react-i18next';
+import { JoinCommunity } from 'components/shared/join_community';
+import { DCTabs } from 'components/shared/tabs';
 
 export function HomeScreen() {
-  const user = useDCStore.use.user();
-  const [all, setAll] = useState<Amity.Post[]>([]);
+  const { t } = useTranslation();
+  const TABS = [
+    { text: t('all'), containerStyle: { flex: 0.7 } },
+    { text: t('festival'), containerStyle: { flex: 1 } },
+    {
+      text: t('competitions'),
+      containerStyle: { flex: 1.6 },
+    },
+    {
+      text: t('class'),
+      containerStyle: { flex: 0.8 },
+    },
+    {
+      text: t('party'),
+      containerStyle: { flex: 0.8, borderBottomWidth: 3 },
+    },
+  ];
+  const [currentTab, setCurrentTab] = useState(TABS[0].text);
 
   return (
     <View style={styles.root}>
-      <HomeEvent all={all} communities={[]} events={[]} user={user} />
+      {isLoading ? (
+        <ActivityIndicator size={'large'} />
+      ) : (
+        <FlatList
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+          style={{ flex: 1 }}
+          data={flatData}
+          ListHeaderComponent={
+            <>
+              <View style={styles.homeTop}>
+                <Text style={styles.homeTitle}>You might be interested</Text>
+                <DCRoundIcon
+                  icon={<RightArrowIcon />}
+                  iconBoxStyle={{
+                    width: 28,
+                    height: 28,
+                    backgroundColor: theming.colors.lightPurple,
+                  }}
+                />
+              </View>
+
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                <HomeItem />
+                <HomeItem />
+              </ScrollView>
+
+              <StartCommunity />
+
+              <Text style={styles.homeEventsTitle}>Your upcoming events</Text>
+
+              <View style={styles.infoHeader}>
+                <DCTabs
+                  textStyle={styles.tabText}
+                  itemStyle={{ alignItems: 'center' }}
+                  scrollEnabled={false}
+                  data={TABS}
+                  currentTab={currentTab}
+                  onPressTab={setCurrentTab}
+                />
+              </View>
+            </>
+          }
+          renderItem={() => null}
+          ListEmptyComponent={<JoinCommunity />}
+          ListFooterComponent={
+            loadingMore ? <ActivityIndicator size={'large'} /> : undefined
+          }
+          keyExtractor={(item, index) =>
+            item?.postId ?? item?.id ?? index.toString()
+          }
+          scrollEventThrottle={500}
+        />
+      )}
     </View>
   );
 }
